3c620e4e1298acbf00afd4e51f33595f28ebef14
Elian Rodriguez <Elian Rodriguez@DESKTOP-G2TP5U0>
2023-08-09 23:16:26 +0200

    Hola

---
+++ Grupo3_Unapec/Models/AreaConocimineto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Grupo3_Unapec.Presentation.Models
+{
+    public class AreaConocimiento
+    {
+        public int ID_Area { get; set; }
+        public int ID_Departamento { get; set; }
+        public string Nombre_Area { get; set; }
+
+        // Constructor sin parámetros
+        public AreaConocimiento()
+        {
+            // Este constructor se utiliza para crear una nueva instancia de AreaConocimiento sin proporcionar valores iniciales.
+        }
+
+        // Constructor con parámetros
+        public AreaConocimiento(int idArea, int idDepartamento, string nombreArea)
+        {
+            // Este constructor se utiliza para crear una nueva instancia de AreaConocimiento con valores iniciales especificados.
+            ID_Area = idArea;
+            ID_Departamento = idDepartamento;
+            Nombre_Area = nombreArea;
+        }
+    }
+}
+
