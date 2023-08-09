2ac3dd0b0302bc958936b471406d35ee26ea1625
Elias Rafael Mercedes Infante <e.mercedes19@unapec.edu.do>
2023-08-09 13:06:08 -0400

    titulacion

---
+++ Grupo3_Unapec/Models/Titulacion.cs
@@ -0,0 +1,26 @@
+﻿namespace Grupo3_Unapec.Presentation.Models
+{
+    public class Titulacion
+    {
+        public int ID_Titulacion { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Duracion { get; set; }
+
+        public Titulacion()
+        {
+           
+        }
+
+        
+        public Titulacion(int id, string nombre, string descripcion, string duracion)
+        {
+            ID_Titulacion = id;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Duracion = duracion;
+        }
+
+    }
+
+}
